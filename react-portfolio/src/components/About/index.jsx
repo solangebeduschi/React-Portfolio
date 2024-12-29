0deca613b080d24d2d0e4bc442211e3df159ea95
Solange Beduschi <solange.beduschi@outlook.com>
2024-12-28 20:37:36 -0500

    first commit

---
+++ react-portfolio/src/components/About/index.jsx
@@ -0,0 +1,19 @@
+function About() {
+  return (
+    <section className="my-5">
+      <div className="my-2">
+        <div className="profile-img my-5">
+        <i className="fas fa-user-circle" style={{ fontSize: "96px" }}></i>
+        </div>
+        <p>
+        A single-page application portfolio for a web developer.
+        </p>
+        <p>
+        This application was built using React.js and Bootstrap.
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default About;
